788adf1eeb2b1639cd03d00f5756fa557d6c973b
MykolaSlichenko <nikolaiiislichenko@gmail.com>
2023-07-26 12:14:03 +0300

    fetch data from api and display it

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'cd6c5dbcc5msh6f4c2ca31552ca1p16cd2fjsnbd6ed2d44ed4',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '3408f69bb895bfe1a8bd4cfc57e71959';
