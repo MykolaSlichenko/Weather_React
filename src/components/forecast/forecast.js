c96dfb6c52e4769f7a1f324089e2fd0f11de444c
MykolaSlichenko <nikolaiiislichenko@gmail.com>
2023-08-07 06:58:33 +0300

    add some classes to modal

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -5,7 +5,7 @@ const Forecast = ({ data }) => {
     console.log('data', data);
 
     const [selectedItem, setSelectedItem] = useState(null);
-  const [forecastByHour, setforecastByHour] = useState(data.list.slice(0, 10));
+    const [forecastByHour, setforecastByHour] = useState(data.list.slice(0, 10));
 
     const handleItemClick = (index) => {
         setSelectedItem(data.list[index]);
@@ -16,7 +16,9 @@ const Forecast = ({ data }) => {
     };
 
   const handleShowMoreItem = () => {
-    setforecastByHour(data.list.slice(0, 20));
+    setforecastByHour(data.list.slice(0, 40));
+    //TODO when click button show more set state from 0 to 40 otherwise 0 to 20
+    // setforecastByHour((prevDaily) => !prevDaily);
   };
 
     return (
@@ -37,11 +39,20 @@ const Forecast = ({ data }) => {
           <button onClick={() => handleShowMoreItem()}>Show More</button>
 
             {selectedItem && (
-                <div key="modal" className="modal">
+              <div className={`modal ${selectedItem ? 'show' : ''}`} key="modal">
                     <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
+                      <div className="daily-details-grid"><div>{selectedItem.dt_txt}</div>
+                        {/*<button className="close" onClick={closeModal}>Close</button>*/}
+                        <div className="close" onClick={closeModal}>&times;</div>
+
                         <div>
-                            <div className="daily-details-grid">
+                          <div className="daily-item">
+                            <img src={`icons/${selectedItem.weather[0].icon}.png`} className="icon-small" alt="weather" />
+                            {/*<label className="day">{selectedItem.dt_txt.slice(5, -3)}</label>*/}
+                            {/*<label className="day">{forecastDays[idx]}</label>*/}
+                            <label className="description">{selectedItem.weather[0].description}</label>
+                            <label className="min-max">{Math.round(selectedItem.main.temp)}°C</label>
+                          </div>
                                 <div className="daily-details-grid-item">
                                     <label>Pressure:</label>
                                     <label>{Math.round(selectedItem.main.pressure * 0.750062)}mmHg</label>
@@ -58,10 +69,10 @@ const Forecast = ({ data }) => {
                                     <label>Wind speed:</label>
                                     <label>{selectedItem.wind.speed} m/s</label>
                                 </div>
-                                <div className="daily-details-grid-item">
-                                    <label>Sea level:</label>
-                                    <label>{selectedItem.main.sea_level}m</label>
-                                </div>
+                                {/*<div className="daily-details-grid-item">*/}
+                                    {/*<label>Sea level:</label>*/}
+                                    {/*<label>{selectedItem.main.sea_level}m</label>*/}
+                                {/*</div>*/}
                                 <div className="daily-details-grid-item">
                                     <label>Feels like:</label>
                                     <label>{selectedItem.main.feels_like}°C</label>
